c6836692b8d37eecc9048ae62a64093a1cf91cd8
Wellington Machado de Espindula <wellington.m.espindula@gmail.com>
2023-12-11 23:30:54 +0100

    Update generate_certs to gen SSH keys

--- generate_certs.sh
+++ generate_certs.sh
@@ -2,3 +2,9 @@
 
 mkdir -p certs
 openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./certs/server.key -out ./certs/server.crt -subj "/CN=FR\/emailAddress=admin@example.com/C=FR/ST=Isere/L=/O=Ensimag/OU=Some Unit" 
+
+mkdir -p .ssh
+ssh-keygen -t rsa -b 4096 -C "admin@example.com" -f .ssh/id_rsa
+chmod 600 ./.ssh/id_rsa
+chmod 644 ./.ssh/id_rsa.pub
+chmod 700 ./.ssh
